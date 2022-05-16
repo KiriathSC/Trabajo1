949d277d5da9de948114e62f48c605a3fe60e142
KiriathSC <93632822+KiriathSC@users.noreply.github.com>
2022-05-15 19:06:58 -0600

    Create abc.go

---
+++ abc.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+func main () {
+	A:= 10
+	B:= 20
+	C:= 30
+	fmt.Println("Ingrese los valores")
+fmt.Scan(&A, &B, &C)
+	resultado:= A*/ B* C*
+	fmt.Println(resultado)
+	
+}
