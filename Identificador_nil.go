a98b1f037bb0e04d42e4badabc01a019a577d8bc
KiriathSC <93632822+KiriathSC@users.noreply.github.com>
2022-05-16 18:52:14 -0600

    Create Identificador_nil.go

---
+++ Identificador_nil.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+	var marvel *int
+
+	if marvel==nil{
+		fmt.Println("marvel", marvel "es nil")
+	}else{
+
+		fmt.Println(marvel, "no es nil")
+	}
+	}
